87332b37b3363429ccf37ec2156603bd6cf964f7
Jiahao Huamani <lucashuamani256w@gmail.com>
2023-11-15 21:45:39 -0500

    Fix score title on mobile

--- src/components/ScoreHeader.jsx
+++ src/components/ScoreHeader.jsx
@@ -3,9 +3,8 @@ import '../styles/ScoreHeader.css';
 const ScoreHeader = ({ currentScore, highScore }) => {
   return (
     <header className="header">
-      <div className="score">
-        CURRENT SCORE: {currentScore} HIGH SCORE: {highScore}
-      </div>
+      <div className="score">CURRENT SCORE: {currentScore}</div>
+      <div className="score">HIGH SCORE: {highScore}</div>
     </header>
   );
 };
