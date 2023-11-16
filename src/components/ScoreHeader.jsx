b922a0f81c43a480238b43ce5a05be15ee4fb6b5
Jiahao Huamani <lucashuamani256w@gmail.com>
2023-11-15 22:36:26 -0500

    Add direction to play game

--- src/components/ScoreHeader.jsx
+++ src/components/ScoreHeader.jsx
@@ -3,8 +3,11 @@ import '../styles/ScoreHeader.css';
 const ScoreHeader = ({ currentScore, highScore }) => {
   return (
     <header className="header">
-      <div className="score">CURRENT SCORE: {currentScore}</div>
-      <div className="score">HIGH SCORE: {highScore}</div>
+      <div className="score-container">
+        <div className="score">CURRENT SCORE: {currentScore}</div>
+        <div className="score">HIGH SCORE: {highScore}</div>
+      </div>
+      <div>Dont Click the same Pokemon twice!</div>
     </header>
   );
 };
